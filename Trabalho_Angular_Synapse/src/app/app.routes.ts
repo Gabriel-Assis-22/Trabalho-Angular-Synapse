f26a21587aa0deac1bfb5e2a1a1e8bd953f8ccef
Gabrie Roberto <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-10-10 14:04:26 -0300

    commit

---
+++ Trabalho_Angular_Synapse/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/register/register';
+import { Home } from './components/home/home';
+
+export const routes: Routes = [
+  {
+    path: '', component: Login
+  },
+  {
+    path: 'register', component: Register
+  },
+  {
+    path: 'home', component: Home
+  }
+];
