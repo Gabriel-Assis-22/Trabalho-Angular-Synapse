b8dbfe5f54e5872210c1cf4d89a11f8ebeb298cb
Gabrie Roberto <152230629+Gabriel-Assis-22@users.noreply.github.com>
2025-10-10 21:08:46 -0300

    commit

--- Trabalho_Angular_Synapse/src/app/components/navbar/navbar.ts
+++ Trabalho_Angular_Synapse/src/app/components/navbar/navbar.ts
@@ -5,8 +5,23 @@ import { RouterModule } from '@angular/router';
   selector: 'app-navbar',
   imports: [RouterModule],
   templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  styleUrl: './navbar.css',
+  template: `
+    <div class="corpo-nav-bar">
+      <a href="#"><img [src]="iconPaths.home" alt="Ícone Home" class="nav-icon"> Home</a>
+      <a href="#"><img [src]="iconPaths.notificacao" alt="Ícone Notificações" class="nav-icon"> Notificações</a>
+      <a href="#"><img [src]="iconPaths.materias" alt="Ícone Matérias" class="nav-icon"> Matérias</a>
+      <a href="#"><img [src]="iconPaths.valores" alt="Ícone Valores" class="nav-icon"> Valores</a>
+      <a href="#"><img [src]="iconPaths.sair" alt="Ícone Sair" class="nav-icon"> Sair</a>
+    </div>
+  `
 })
 export class Navbar {
-  
+  iconPaths = {
+    home: '/botao-home.png',
+    notificacao: '/notificacao.png',
+    materias: '/livro.png',
+    valores: '/estrela.png',
+    sair: '/assinar-em.png',
+  };
 }
